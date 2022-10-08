8e5838be2eed91544656fe557e14c27bf1c8d6a5
neeraj001122 <admobid0011>
2022-10-08 19:01:15 +0530

    reacttask9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Tajmahal",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Japan",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Dubai",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "America",
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          location={item.location}
+        ></ExpenseItem>
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() 
+function ExpenseItem(props) 
 {
-    var food  = <div className='expense-item'>
-            <div>Expense Item</div>
-            <div className='expense-item__description'><h2>Food</h2>
-            <div className='expense-item__price'>10</div>
-            </div>
+    return(
+         <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'><h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.location}</div>   
+        <div className='expense-item__price'>{props.amount}</div>
         </div>
-
-    var petrol  = <div className='expense-item'>
-            <div>Expense Item</div>
-            <div className='expense-item__description'><h2>petrol</h2>
-            <div className='expense-item__price'>100</div>
-            </div>
-        </div> 
-        
-        var movie  = <div className='expense-item'>
-        <div>Expense Item</div>
-        <div className='expense-item__description'><h2>Movie</h2>
-        <div className='expense-item__price'>200</div>
-        </div>
-    </div>    
-   var arr = [food,petrol,movie]
-   return arr       
+    </div>
+    );          
 }
 
 export default ExpenseItem;
