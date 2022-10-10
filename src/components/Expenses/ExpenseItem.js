96c04b2e285386d63dbbed8bbb9a04eba8c482fd
neeraj001122 <admobid0011>
2022-10-10 11:03:15 +0530

    adreacttask2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,17 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => 
 {
+    const clickHandler = () => {
+        let items = document.getElementById('yo!')
+        let div = document.getElementById('div')
+        div.remove(div)
+        console.log(items)
+    };
     return(
-         <div className='expense-item'>
+         <div className='expense-item' id='div'>
          <ExpenseDate date={props.date}></ExpenseDate>
          <ExpenseDetails amount={props.amount} title={props.title} location={props.location} ></ExpenseDetails>
+         <button onClick={clickHandler}>Delete</button>
         </div>
     );          
 }
