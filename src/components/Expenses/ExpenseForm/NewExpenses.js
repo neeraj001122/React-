7886a20018e109ee87d37d33340371f1fabd729d
neeraj001122 <admobid0011>
2022-10-11 23:20:37 +0530

    adreacttask5

--- src/components/Expenses/ExpenseForm/NewExpenses.js
+++ src/components/Expenses/ExpenseForm/NewExpenses.js
@@ -1,22 +1,40 @@
+import React, {useState} from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 import "./NewExpenses.css";
 const NewExpenses = (props) => {
-  const data = (event) => {
+  const [oldTitle, newTitle] = useState('')
+  const [oldAmount, newAmount] = useState('')
+  const [oldDAte, newDate] = useState('')
+
+  const TitalChange = (event) => {
     console.log(event.target.value)
+   newTitle(event.target.value)
   };
+
+  
+  const AmountChange = (event) => {
+   newAmount(event.target.value)
+  };
+
+  
+   const DateChange = (event) => {
+   newDate(event.target.value)
+  };
+
   return (
     <form>
       <div className="new-Expense__controls">
         <div className="new-expense__control">
           <label>Title:- </label>
-          <input type="text"  onChange={data}/>
+          <input type="text"  onChange={TitalChange}/>
         </div>
         <div className="new-expense__control">
           <label>Amount:- </label>
-          <input type="number" onChange={data}/>
+          <input type="number" onChange={AmountChange}/>
         </div>
         <div className="new-expense__control">
           <label>Date:- </label>
-          <input type="date" onChange={data}/>
+          <input type="date" onChange={DateChange}/>
         </div>
       </div>
       <div className="new-expense__actions">
