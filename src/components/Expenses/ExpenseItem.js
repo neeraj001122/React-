5d3d65d406ebe359b543bb8586c82fc0bdf4e173
neeraj001122 <admobid0011>
2022-10-11 14:23:51 +0530

    adracttask3

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
+import React, {useState} from 'react'
+
 import ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => 
 {
+    let [old, setAmount] = useState(props.amount);
+    const update = () => {
+        setAmount('$100')
+        console.log('clicked')
+    };
     const clickHandler = () => {
         let items = document.getElementById('yo!')
         let div = document.getElementById('div')
@@ -13,8 +20,9 @@ const ExpenseItem = (props) =>
     return(
          <div className='expense-item' id='div'>
          <ExpenseDate date={props.date}></ExpenseDate>
-         <ExpenseDetails amount={props.amount} title={props.title} location={props.location} ></ExpenseDetails>
-         <button onClick={clickHandler}>Delete</button>
+         <ExpenseDetails amount={old} title={props.title} location={props.location} ></ExpenseDetails>
+         <button className='expense-button' onClick={clickHandler}>Delete</button>
+         <button className='expense-button' onClick={update}>Update</button>
         </div>
     );          
 }
